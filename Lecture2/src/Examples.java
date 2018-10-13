ec5f921eb610b93a0b64d15c72bd45500495bb40
Acer\Aspire <cherryy552@gmail.com>
2018-10-13 16:28:53 +0630

    added read and write character

--- Lecture2/src/Examples.java
+++ Lecture2/src/Examples.java
@@ -1,8 +1,12 @@
 import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.OutputStream;
 import java.io.Reader;
+import java.io.Writer;
 
 public class Examples {
 
@@ -11,7 +15,11 @@ public class Examples {
 //		readOneByte();
 //		readOneChar();
 //		readArrayOfByte();
-		readArrayOfChar();
+//		readArrayOfChar();
+		// writeByte();
+//		readAndWriteBytes();
+//		writeChar();
+		readAndWriteChars();
 	}
 
 	private static void readOneByte() {
@@ -105,7 +113,128 @@ public class Examples {
 				e.printStackTrace();
 			}
 		}
+	}
 
+	private static void writeByte() {
+		OutputStream output = null;
+		try {
+			output = new FileOutputStream(
+					"C:\\Su Cherry Wine\\workspaceSEadvance\\SEAdvanceLecture2\\Lecture2\\writebytes.txt");
+			byte byteVal = 100;
+			output.write(byteVal);
+
+			byte[] byteVal1 = { 0, 63, 127 };
+			output.write(byteVal1);
+
+			output.flush();
+		} catch (IOException e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				output.close();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+	}
+
+	private static void readAndWriteBytes() {
+		InputStream input = null;
+		OutputStream output = null;
+		try {
+			input = new FileInputStream(
+					"C:\\Su Cherry Wine\\workspaceSEadvance\\SEAdvanceLecture2\\Lecture2\\bytes.txt");
+			output = new FileOutputStream(
+					"C:\\Su Cherry Wine\\workspaceSEadvance\\SEAdvanceLecture2\\Lecture2\\readandwritebytes.txt");
+			int intVal;
+			while ((intVal = input.read()) >= 0) {
+				byte byteVal = (byte) intVal;
+				output.write(byteVal);
+				output.flush();
+			}
+
+			byte[] byteBuff = new byte[10];
+			int length;
+			while ((length = input.read(byteBuff)) >= 0) {
+				for (int i = 0; i < length; i++) {
+					byte byteVal = byteBuff[i];
+					output.write(byteVal);
+				}
+			}
+
+		} catch (IOException e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				input.close();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+	}
+
+	private static void writeChar() {
+		Writer writer = null;
+		try {
+			writer = new FileWriter(
+					"C:\\Su Cherry Wine\\workspaceSEadvance\\SEAdvanceLecture2\\Lecture2\\writeonechars.txt");
+			char charVal = 'a';
+			writer.write(charVal);
+
+			char[] charbuff = { 'a', 'b', 'c' };
+			writer.write(charbuff);
+
+			String stringVal = "Hello World";
+			writer.write(stringVal);
+		} catch (IOException e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				writer.close();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+	}
+
+	private static void readAndWriteChars() {
+		InputStream input = null;
+		OutputStream output = null;
+		Writer writer = null;
+		try {
+			input = new FileInputStream(
+					"C:\\Su Cherry Wine\\workspaceSEadvance\\SEAdvanceLecture2\\Lecture2\\bytes.txt");
+			writer = new FileWriter(
+					"C:\\Su Cherry Wine\\workspaceSEadvance\\SEAdvanceLecture2\\Lecture2\\writeandwritechars.txt");
+			int intVal;
+			while ((intVal = input.read()) >= 0) {
+				char charVal = 'a';
+				writer.write(charVal);
+				writer.flush();
+			}
+
+			byte[] byteBuff = new byte[10];
+			int length;
+			while ((length = input.read(byteBuff)) >= 0) {
+				for (int i = 0; i < length; i++) {
+					byte byteVal = byteBuff[i];
+					output.write(byteVal);
+				}
+			}
+
+		} catch (IOException e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				input.close();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
 	}
 
 }
