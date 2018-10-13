36343aa35d6f52a5e23005189e54e44696d65e2c
Acer\Aspire <cherryy552@gmail.com>
2018-10-13 15:21:46 +0630

    added array byte and charcter reading method

--- Lecture2/src/Examples.java
+++ Lecture2/src/Examples.java
@@ -9,33 +9,73 @@ public class Examples {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 //		readOneByte();
-		readOneChar();
+//		readOneChar();
+//		readArrayOfByte();
+		readArrayOfChar();
 	}
-	
+
 	private static void readOneByte() {
 		try {
-			InputStream input = new FileInputStream("C:\\Su Cherry Wine\\workspaceSEadvance\\SEAdvanceLecture2\\Lecture2\\bytes.txt");
-		int intVal;
-		while ((intVal = input.read()) >= 0) {
-			byte byteVal = (byte) intVal;
-			System.out.print(byteVal);
-		}
+			InputStream input = new FileInputStream(
+					"C:\\Su Cherry Wine\\workspaceSEadvance\\SEAdvanceLecture2\\Lecture2\\bytes.txt");
+			int intVal;
+			while ((intVal = input.read()) >= 0) {
+				byte byteVal = (byte) intVal;
+				System.out.print(byteVal);
+			}
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
 	}
-	
+
 	private static void readOneChar() {
 		try {
-			Reader reader = new FileReader("C:\\Su Cherry Wine\\workspaceSEadvance\\SEAdvanceLecture2\\Lecture2\\chars.txt");
-		int intVal;
-		while ((intVal = reader.read()) >= 0) {
-			char charVal = (char) intVal;
-			System.out.print(charVal);
-		}
+			Reader reader = new FileReader(
+					"C:\\Su Cherry Wine\\workspaceSEadvance\\SEAdvanceLecture2\\Lecture2\\chars.txt");
+			int intVal;
+			while ((intVal = reader.read()) >= 0) {
+				char charVal = (char) intVal;
+				System.out.print(charVal);
+			}
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
+	}
+
+	private static void readArrayOfByte() {
+		try {
+			InputStream input = new FileInputStream(
+					"C:\\Su Cherry Wine\\workspaceSEadvance\\SEAdvanceLecture2\\Lecture2\\bytes.txt");
+			int length;
+			byte[] byteBuff = new byte[10];
+			while ((length = input.read(byteBuff)) >= 0) {
+				for (int i = 0; i < length; i++) {
+					byte byteVal = byteBuff[i];
+					System.out.print(byteVal);
+				}
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+	}
+
+	private static void readArrayOfChar() {
+		try {
+			Reader input = new FileReader(
+					"C:\\Su Cherry Wine\\workspaceSEadvance\\SEAdvanceLecture2\\Lecture2\\chars.txt");
+			int length;
+			char[] charBuff = new char[10];
+			while ((length = input.read(charBuff)) >= 0) {
+				for (int i = 0; i < length; i++) {
+					char charVal = charBuff[i];
+					System.out.print(charVal);
+				}
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
 	}
 
 }
