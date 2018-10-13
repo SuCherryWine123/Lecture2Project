4dccbba23e3b087960cc5756386745668620632c
Acer\Aspire <cherryy552@gmail.com>
2018-10-13 14:45:45 +0630

    added character method in file

--- Lecture2/src/Examples.java
+++ Lecture2/src/Examples.java
@@ -1,21 +1,37 @@
 import java.io.FileInputStream;
+import java.io.FileReader;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.Reader;
 
 public class Examples {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		readOneByte();
+//		readOneByte();
+		readOneChar();
 	}
 	
 	private static void readOneByte() {
 		try {
-			InputStream input = new FileInputStream("C:\\Su Cherry Wine\\JavaSE Basic Platform\\Streams\\bytes.txt");
+			InputStream input = new FileInputStream("C:\\Su Cherry Wine\\workspaceSEadvance\\SEAdvanceLecture2\\Lecture2\\bytes.txt");
 		int intVal;
 		while ((intVal = input.read()) >= 0) {
 			byte byteVal = (byte) intVal;
 			System.out.print(byteVal);
+		}
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	private static void readOneChar() {
+		try {
+			Reader reader = new FileReader("C:\\Su Cherry Wine\\workspaceSEadvance\\SEAdvanceLecture2\\Lecture2\\chars.txt");
+		int intVal;
+		while ((intVal = reader.read()) >= 0) {
+			char charVal = (char) intVal;
+			System.out.print(charVal);
 		}
 		} catch (IOException e) {
 			e.printStackTrace();
